b445f8152844942a03e42b3c6ac592086033f219
jawnsunn <jawnsunn@gmail.com>
2019-05-16 15:10:18 -0700

    Created hitbox and collision flags for P1/P2 hitboxes(p1_attack/p2_attack).
    Need to display score for both players and a reset function that resets both players into neutral space positions

--- Game.cpp
+++ Game.cpp
@@ -31,18 +31,28 @@ Game::Game(){
 	fireballFileName = "fireball.bmp";
 	#endif
 
-    mushroom = new TexRect(shroomFileName, -0.25, 0.5, 0.5, 0.5);
-    //projectile = new Rect(-0.05, -0.8, 0.1, 0.1);
-	projectile = new AnimatedRect(playerFileName, 2, 5, 60, true, true, -0.05, 0, 1, 1);
+    //mushroom = new TexRect(shroomFileName, -0.25, 0.5, 0.5, 0.5);
+    Player1 = new Rect(-0.5, -0.50, 0.1, 0.25);
+	Player2 = new Rect(0.5, -0.50, 0.1, 0.25, 1.0, 0, 0);
+	//Player1 = new AnimatedRect(playerFileName, 2, 5, 60, true, true, -0.05, 0, 1, 1);
     explosion = new AnimatedRect(fireballFileName, 6, 6, 20, false, false, -0.25, 0.8, 0.5, 0.5);
     
     up = false;
     left = true;
-	moveLeft = false;
-	moveRight = false;
-    projectileVisible = true;
-	player_fwdwalkVisible = false;
-    mushroomVisible = true;
+
+	p1_moveLeft = false;
+	p1_moveRight = false;
+	p2_moveLeft = false;
+	p2_moveRight = false;
+
+    player1Visible = true;
+	p1_attackVisible = false;
+	player2Visible = true;
+	p2_attackVisible = false;
+    mushroomVisible = false;
+
+	p1_canAttack = false;
+	p2_canAttack = false;
     theta = 0;
     deg = 0;
     hit = false;
@@ -52,6 +62,7 @@ Game::Game(){
 }
 
 void Game::action(){
+	/*
     float mx = 0.5;
     float my = 0;
     
@@ -60,7 +71,7 @@ void Game::action(){
     
     mx = 0.5 * cos(theta);
     my = 0.5 * sin(theta);
-    
+    */
 //    if (left)
 //        mx -= 0.0005;
 //    else
@@ -73,19 +84,21 @@ void Game::action(){
 //        left = true;
 //    }
     
-    
+    /*
     mushroom->setX(mx - mushroom->getW()/2);
     mushroom->setY(my + mushroom->getH()/2);
     
     theta += 0.001;
     deg += 0.1;
-    
+    */
 	
+	/*
     if (!hit && up){
         float ypos = projectile->getY();
         ypos +=0.001;
         projectile->setY(ypos);
-
+	
+		
         if (mushroom->contains(0, ypos-0.005)){
             up = false;
             hit = true;
@@ -94,46 +107,93 @@ void Game::action(){
             explosion->setX(mushroom->getX());
             explosion->setY(mushroom->getY());
             explosion->playOnce();
-        }
-    } 
+        } 
+    } */
 
 	
-	if (moveLeft) {
-		projectile->setMap("../ryu_fwdwlk.png", 2, 5);
-		float xpos = projectile->getX();
+	if (p1_moveLeft) {
+		//projectile->setMap("../ryu_fwdwlk.png", 2, 5);
+		float xpos = Player1->getX();
 		xpos -= 0.005;
-		projectile->setX(xpos);
-		moveLeft = false;
+		Player1->setX(xpos);
+		p1_moveLeft = false;
 	}
-	else if (moveRight) {
-		projectile->setMap("../ryu_backwlk.png", 2, 5);
+	else if (p1_moveRight) {
+		//projectile->setMap("../ryu_backwlk.png", 2, 5);
 		//player_fwdwalkVisible = true;
-		float xpos = projectile->getX();
+		float xpos = Player1->getX();
 		xpos += 0.005;
-		moveRight = false;
+		Player1->setX(xpos);
+		p1_moveRight = false;
 	}
-	else {
-		float xpos = projectile->getX();
-		projectile->setX(xpos);
-	} 
+
+	if (p1_canAttack) {
+		p1_attackVisible = true;
+		float atk1pos = Player1->getX();
+		p1_attack = new Rect(atk1pos, -0.5, 0.4, 0.2, 0.0, 0.5, 0.0);
+		atk1pos += 0.1;
+		p1_attack->setX(atk1pos);
+		if (Player2->contains(atk1pos+0.5, -0.5) && player2Visible) {
+				player2Visible = false;
+				p1_attackVisible = false;
+				delete p1_attack;
+				explosion->setX(Player2->getX());
+				explosion->setY(Player2->getY());
+				explosion->playOnce();
+		}
+		p1_attackVisible = false;
+	}
     
+	if (p2_moveLeft) {
+		float xpos2 = Player2->getX();
+		xpos2 -= 0.005;
+		Player2->setX(xpos2);
+		p2_moveLeft = false;
+	}
+	else if (p2_moveRight) {
+		float xpos2 = Player2->getX();
+		xpos2 += 0.005;
+		Player2->setX(xpos2);
+		p2_moveRight = false;
+	}
+
+	if (p2_canAttack) {
+		p2_attackVisible = true;
+		float atk2pos = Player2->getX();
+		p2_attack = new Rect(atk2pos, -0.5, 0.4, 0.2, 0.0, 0.5, 0.0);
+		atk2pos -= 0.3;
+		p2_attack->setX(atk2pos);
+		if (Player1->contains(atk2pos, -0.5) && player1Visible) {
+			player1Visible = false;
+			p2_attackVisible = false;
+			delete p2_attack;
+			explosion->setX(Player1->getX());
+			explosion->setY(Player1->getY());
+			explosion->playOnce();
+		}
+		p2_attackVisible = false;
+	}
+	/*
     if (hit){
         explosion->setY(explosion->getY()-0.0001);
-    }
+    } */
 }
 
 void Game::draw() const {
     
-    if (projectileVisible){
+    if (player1Visible){
 		//projectile->setMap("ryu_idle.png", 2, 5);
-        projectile->draw(0.1);
+        Player1->draw();
     }
-    if (mushroomVisible){
-        mushroom->draw(0.0);
-    }
-	if (player_fwdwalkVisible) {
-		projectile->draw(0.1);
+	if (player2Visible) {
+		Player2->draw();
 	}
+    if (p1_attackVisible){
+		p1_attack->draw();
+    }
+
+	if (p2_attackVisible)
+		p2_attack->draw();
     explosion->draw(0.1);
 }
 
@@ -149,19 +209,47 @@ void Game::handleKeyDown(unsigned char key, float x, float y){
     }
 
 	if (key == 'a') {
-		moveLeft = true;
+		p1_moveLeft = true;
 	}
-	if (key == 's') {
-		moveRight = true;
+	else if (key == 's') {
+		p1_moveRight = true;
 	}
+	
+	if (key == 'd' && p1_attackVisible == false)
+		p1_canAttack = true;
+
+	if (key == 'j') {
+		p2_moveLeft = true;
+	}
+	else if (key == 'k') {
+		p2_moveRight = true;
+	}
+
+	if (key == 'l' && p2_attackVisible == false)
+		p2_canAttack = true;
 }
 
 void Game::handleKeyUp(unsigned char key, float x, float y) {
 	if (key == 'a') {
-		moveLeft = false;
+		p1_moveLeft = false;
 	}
-	if (key == 's') {
-		moveRight = false;
+	else if (key == 's') {
+		p1_moveRight = false;
+	}
+
+	if (key == 'd' == p1_attackVisible == true) {
+		p1_canAttack = false;
+	}
+
+	if (key == 'j') {
+		p2_moveLeft = false;
+	}
+	else if (key == 'k') {
+		p2_moveRight = false;
+	}
+
+	if (key == 'l' && p2_attackVisible == true) {
+		p1_canAttack = false;
 	}
 }
 
@@ -169,5 +257,7 @@ Game::~Game(){
     stop();
     delete mushroom;
     delete explosion;
-    delete projectile;
+    delete Player1;
+	delete Player2;
+	delete p1_attack;
 }
--- Game.h
+++ Game.h
@@ -8,16 +8,33 @@
 
 class Game: public AppComponent, private Timer{
     TexRect* mushroom;
-    AnimatedRect* projectile;
-	AnimatedRect* player_fwdwalk;
+    //AnimatedRect* Player1;
+	Rect* Player1;
+	Rect* Player2;
+	Rect* p1_attack;
+	Rect* p2_attack;
+
+	int player1_score;
+	int player2_score;
     
-    bool projectileVisible;
-	bool player_fwdwalkVisible;
+    bool player1Visible;
+	bool player2Visible;
+	//bool player_fwdwalkVisible;
     bool mushroomVisible;
+	bool p1_attackVisible;
+	bool p2_attackVisible;
+
     bool up;
     bool left;
-	bool moveLeft;
-	bool moveRight;
+
+	bool p1_moveLeft;
+	bool p1_moveRight;
+	bool p2_moveLeft;
+	bool p2_moveRight;
+
+	bool p1_canAttack;
+	bool p2_canAttack;
+
     bool hit;
     float theta;
     float deg;
